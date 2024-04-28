028dd4d1409276175b564b2b98e0a4c1c4f5c83b
kimberlycoppola <95484520+kimberlycoppola@users.noreply.github.com>
2024-04-28 22:14:53 +0200

    Sito

---
+++ slideshow.js
@@ -0,0 +1,25 @@
+// Variabili per tenere traccia dell'indice dell'immagine corrente
+let currentDrawingIndex = 0;
+let currentPhotoIndex = 0;
+
+// Array delle immagini e delle loro rispettive sorgenti
+const drawings = ["immagini_portfolio/immagine1.jpg", "immagini_portfolio/immagine2.jpg", "immagini_portfolio/immagine3.jpg"]; // Aggiungi qui tutte le tue immagini per i disegni
+const photos = ["venezia/venezia2.jpeg", "praga/praga4.jpeg", "berlino/cracovia1.jpeg"]; // Aggiungi qui tutte le tue immagini per le foto
+
+// Funzione per passare all'immagine successiva per i disegni
+function nextDrawing() {
+    currentDrawingIndex++;
+    if (currentDrawingIndex >= drawings.length) {
+        currentDrawingIndex = 0;
+    }
+    document.getElementById("drawing").src = drawings[currentDrawingIndex];
+}
+
+// Funzione per passare all'immagine successiva per le foto
+function nextPhoto() {
+    currentPhotoIndex++;
+    if (currentPhotoIndex >= photos.length) {
+        currentPhotoIndex = 0;
+    }
+    document.getElementById("photo").src = photos[currentPhotoIndex];
+}
